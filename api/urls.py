6d3122a203ab9a153f59f61580ce1a167079c517
Shudhamsu <shresthashudhamsu@gmail.com>
2022-04-29 09:20:24 +1000

    enter

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('pay/', startPayment.as_view(), name="payment"),
+    # path('pay/', startPayment.as_view(), name="payment"),
+    path('pay/', start_payment, name="payment"),  
     # path('pay/<int:pk>',paymentnumber.as_view()),
-    # path('payment/success/', handle_payment_success, name="payment_success")
+    path('payment/success/', handle_payment_success, name="payment_success")
 ]
--- api/views.py
+++ api/views.py
@@ -16,33 +16,19 @@ env = environ.Env()
 environ.Env.read_env()
 
 
-# @api_view(['GET','POST'])
-# def start_payment(request):
-
-#     if request.method == 'GET':
-#         orders = Order.objects.all()
-#         serializer = OrderSerializer(orders, many=True)
-#         return Response(serializer.data)
-    
-
-#     elif request.method == 'POST':
-#         serializer = OrderSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
-
-class startPayment(ListCreateAPIView):
-    queryset = Order.objects.none()
-    serializer_class = OrderSerializer
+# class startPayment(ListCreateAPIView):
+#     queryset = Order.objects.none()
+#     serializer_class = OrderSerializer
 
     # def get(self, request, *args, **kwargs):
     #     return Response()
 
     # def post(self, request, *args, **kwargs):
-        
+        ## This data will come from frontend 
     #     amount = request.data['order_amount']
     #     name = request.data['order_product']
+    
+    ##  This is the client to whom the user is paying
     #     client = razorpay.Client(auth=(env('PUBLIC_KEY'), env('SECRET_KEY')))
         
     #     # try:
@@ -59,21 +45,19 @@ class startPayment(ListCreateAPIView):
     #     #  return Response(serializer.data,status=status.HTTP_400_BAD_REQUEST)
 
  
-        
-class paymentnumber(RetrieveUpdateDestroyAPIView):
-        queryset = Order.objects.all()
-        serializer_class = OrderSerializer
+# This will be able to find objects based on their primary number      
+# class paymentnumber(RetrieveUpdateDestroyAPIView):
+#         queryset = Order.objects.all()
+#         serializer_class = OrderSerializer
         
 
 # @api_view(['GET','POST'])
 # def start_payment(request):
 
 #     if request.method == 'POST':
-#         name = Order.objects.filter(order_product=request.data['order_product'])
-#         amount = Order.objects.filter(order_amount=request.data['order_amount'])
-        
+#         name = request.POST.get("order_product")
+#         amount = request.POST.get("order_amount")
 #         client = razorpay.Client(auth=(env('PUBLIC_KEY'), env('SECRET_KEY')))
-         
 #         payment = client.order.create({"amount": int(amount) * 100, 
 #                                    "currency": "INR", 
 #                                    "payment_capture": "1"})
@@ -82,4 +66,107 @@ class paymentnumber(RetrieveUpdateDestroyAPIView):
 #                                  order_payment_id=payment['id'])
 #         serializer = OrderSerializer(order)
         
-#         return Response(serializer)
+#         # data = {
+#         # "payment": payment,
+#         # "order": serializer.data
+#         # }
+#         return Response(serializer.data)
+
+
+@api_view(['POST'])
+def start_payment(request):
+    # request.data is coming from frontend
+    amount = request.data['amount']
+    name = request.data['name']
+
+    # setup razorpay client this is the client to whome user is paying money that's you
+    client = razorpay.Client(auth=(env('PUBLIC_KEY'), env('SECRET_KEY')))
+
+    # create razorpay order
+    # the amount will come in 'paise' that means if we pass 50 amount will become
+    # 0.5 rupees that means 50 paise so we have to convert it in rupees. So, we will 
+    # mumtiply it by 100 so it will be 50 rupees.
+    payment = client.order.create({"amount": int(amount) * 100, 
+                                   "currency": "INR", 
+                                   "payment_capture": "1"})
+
+    # we are saving an order with isPaid=False because we've just initialized the order
+    # we haven't received the money we will handle the payment succes in next 
+    # function
+    order = Order.objects.create(order_product=name, 
+                                 order_amount=amount, 
+                                 order_payment_id=payment['id'])
+
+    serializer = OrderSerializer(order)
+
+    """order response will be 
+    {'id': 17, 
+    'order_date': '23 January 2021 03:28 PM', 
+    'order_product': '**product name from frontend**', 
+    'order_amount': '**product amount from frontend**', 
+    'order_payment_id': 'order_G3NhfSWWh5UfjQ', # it will be unique everytime
+    'isPaid': False}"""
+
+    data = {
+        "payment": payment,
+        "order": serializer.data
+    }
+    return Response(data)
+
+
+@api_view(['POST'])
+def handle_payment_success(request):
+    # request.data is coming from frontend
+    res = json.loads(request.data["response"])
+
+    """res will be:
+    {'razorpay_payment_id': 'pay_G3NivgSZLx7I9e', 
+    'razorpay_order_id': 'order_G3NhfSWWh5UfjQ', 
+    'razorpay_signature': '76b2accbefde6cd2392b5fbf098ebcbd4cb4ef8b78d62aa5cce553b2014993c0'}
+    this will come from frontend which we will use to validate and confirm the payment
+    """
+
+    ord_id = ""
+    raz_pay_id = ""
+    raz_signature = ""
+
+    # res.keys() will give us list of keys in res
+    for key in res.keys():
+        if key == 'razorpay_order_id':
+            ord_id = res[key]
+        elif key == 'razorpay_payment_id':
+            raz_pay_id = res[key]
+        elif key == 'razorpay_signature':
+            raz_signature = res[key]
+
+    # get order by payment_id which we've created earlier with isPaid=False
+    order = Order.objects.get(order_payment_id=ord_id)
+
+    # we will pass this whole data in razorpay client to verify the payment
+    data = {
+        'razorpay_order_id': ord_id,
+        'razorpay_payment_id': raz_pay_id,
+        'razorpay_signature': raz_signature
+    }
+
+    client = razorpay.Client(auth=(env('PUBLIC_KEY'), env('SECRET_KEY')))
+
+    # checking if the transaction is valid or not by passing above data dictionary in 
+    # razorpay client if it is "valid" then check will return None
+    check = client.utility.verify_payment_signature(data)
+
+    if check is not None:
+        print("Redirect to error url or error page")
+        return Response({'error': 'Something went wrong'})
+
+    # if payment is successful that means check is None then we will turn isPaid=True
+    order.isPaid = True
+    order.save()
+
+    res_data = {
+        'message': 'payment successfully received!'
+    }
+
+    return Response(res_data)
+
+
